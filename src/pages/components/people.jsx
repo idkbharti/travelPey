3e19e13f5565ce23fa52a0169b54f1da888be691
Dev Kumar <nubdevs>
2023-04-24 22:39:48 +0530

    final commit

--- src/pages/components/people.jsx
+++ src/pages/components/people.jsx
@@ -22,7 +22,7 @@ const people = () => {
       <div className="text-4xl font-serif font-bold text-[#294840]">
         What do people feel...
       </div>
-      <div className="p-10 w-full flex flex-row justify-between gap-x-20">
+      <div className="p-10 w-full flex flex-row justify-between gap-x-20 mt-20">
         {people.map((card, i) => (
           <div key={i} className="flex flex-row items-center justify-between gap-3">
             <div className="rounded-full">
--- src/pages/components/popular.jsx
+++ src/pages/components/popular.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
@@ -17,20 +18,22 @@ const popular = () => {
             <div className='text-4xl font-serif font-bold text-[#294840]'>Popular Flights near you</div>
         <div className='text-[#294840] font-serif text-xl'>Find dashing deals on domestic and international flights!</div>
   <div className='relative flex items-center bg-white w-full py-5 justify-center'>
-    <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
+    <MdChevronLeft className='opacity-50 text-blue-600 cursor-pointer hover:opacity-100' onClick={slideLeft} size={50} />
     <div
       id='slider'
       className='w-[85%]  h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'
     >
       {data.map((item,i) => (
-        <img key={i}
-          className='w-[220px] inline-block p-2 rounded-md cursor-pointer hover:scale-105 ease-in-out duration-300'
+        <Image key={i}
+        width={220}
+        height={500}
+          className='inline-block p-2 rounded-md cursor-pointer hover:scale-105 ease-in-out duration-300'
           src={item}
           alt='/'
         />
       ))}
     </div>
-    <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} size={40} />
+    <MdChevronRight className='opacity-50 text-blue-600 cursor-pointer hover:opacity-100' onClick={slideRight} size={50} />
   </div>
 </div>
   )
